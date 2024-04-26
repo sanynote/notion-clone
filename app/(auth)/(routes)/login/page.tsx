d5747ac9ef6c8a21cde8178443ab103c8f9ac48c
sanynote <sunnyjuice94@gmail.com>
2024-04-26 21:07:17 +0900

    folders setup

---
+++ app/(auth)/(routes)/login/page.tsx
@@ -0,0 +1,8 @@
+import { Button } from "@/components/ui/button";
+export default function LoginPage() {
+  return (
+    <div>
+      <Button>login</Button>
+    </div>
+  )
+}
---
+++ app/(auth)/(routes)/register/page.tsx
@@ -0,0 +1,8 @@
+import { Button } from "@/components/ui/button";
+export default function RegisterPage() {
+  return (
+    <div>
+      <Button>register</Button>
+    </div>
+  )
+}
