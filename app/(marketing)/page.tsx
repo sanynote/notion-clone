08d34086d8c44a9c90ccd8e08ff65cfbeab78376
sanynote <sunnyjuice94@gmail.com>
2024-05-05 21:53:03 +0900

    Landing page ..

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,4 +1,5 @@
 import { Heading } from "./_components/heading";
+import { Heroes } from "./_components/heroes";
 
 const MarketingPage = () => {
   return (
@@ -8,6 +9,7 @@ const MarketingPage = () => {
       md:justify-start text-center gap-y-8 flex-1 px-6 pb-10"
       >
         <Heading />
+        <Heroes />
       </div>
     </div>
   );
